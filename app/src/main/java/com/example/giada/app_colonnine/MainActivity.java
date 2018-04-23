0c184cbeae1bae85281a41a5d7430b835e3ab11a
Giada Mastropietro <giada.mastropietro@studenti.unicampania.it>
2018-04-23 15:56:00 +0200

    Terzo commit

--- app/src/main/java/com/example/giada/app_colonnine/MainActivity.java
+++ app/src/main/java/com/example/giada/app_colonnine/MainActivity.java
@@ -3,11 +3,72 @@ package com.example.giada.app_colonnine;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.content.ClipData;
+        import android.content.Intent;
+        import android.support.annotation.NonNull;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+        import com.google.firebase.auth.FirebaseUser;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    // Costanti
+    private final static String EXTRA_COLONNINE = "colonnine";
+    private final static String TAG = "AppColonnine";
+
+    // Widget
+    private ListView listaColonnine;
+
+
+    // Adapter
+    private ColonninaAdapter adapter;
+
+
+    // Autenticazione Firebase
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getResources().getStringArray(R.array.supporti);
+
+
+        // Autenticazione Firebase
+        mAuth = FirebaseAuth.getInstance();
+
+        // Comportamento differenziato
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null) {
+            // Utente non autenticato, voglio impedire l'accesso
+            Intent intent = new Intent(this, login.class);
+            startActivity(intent);
+        } else {
+            // Utente autenticato.
+            // Nessuna operazione richiesta
+        }
+
+
+
+
+
+    Colonnina c = new Colonnina();
+        c.setIndirizzogenerico("viale Europa");
+        c.setGestore("Enel");
     }
+        //c.setSupporto(TipoSupporto.values());
+
+
+
+
 }
--- app/src/main/java/com/example/giada/app_colonnine/login.java
+++ app/src/main/java/com/example/giada/app_colonnine/login.java
@@ -1,48 +1,59 @@
 package com.example.giada.app_colonnine;
 
-import android.os.Bundle;
+
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
-public class login_activity1 extends AppCompatActivity {
+
+
+
+public class login extends AppCompatActivity {
+
     // Costanti
     private final static String TAG = "Login";
+
+
     // Widget
     private EditText mUsername;
-    private EditText mPassword;
-    private Button btnLogin;
-    private ProgressBar mProgress;
+    private EditText mPassw;
+    private ImageButton btnLogin;
+
+
 
     // Firebase
     private FirebaseAuth mAuth;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_activity1);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
         // Imposto gli attributi relativi ai widget
         mUsername = (EditText)findViewById(R.id.editUsername);
-        mPassword = (EditText)findViewById(R.id.editPassw);
-        btnLogin = (Button)findViewById(R.id.Button);
-        mProgress = (ProgressBar)findViewById(R.id.progressBar);
+        mPassw = (EditText)findViewById(R.id.editPassw);
+        btnLogin = (ImageButton)findViewById(R.id.btnLogin);
+
 
         // Impostazione Firebase
         mAuth = FirebaseAuth.getInstance();
@@ -51,52 +62,41 @@ public class login_activity1 extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = mUsername.getText().toString();
-                String password = mPassword.getText().toString();
-                if (username.isEmpty())
+                String email = mUsername.getText().toString();
+                String password = mPassw.getText().toString();
+                if (email.isEmpty())
                     mUsername.setError(getString(R.string.obbligatorio));
                 else if (password.isEmpty())
-                    mPassword.setError(getString(R.string.obbligatorio));
+                    mPassw.setError(getString(R.string.obbligatorio));
                 else
-                    effettuaLogin(username, password);
+                    effettuaLogin(email, password);
             }
         });
     }
 
     /**
      * Funzione che prova ed effettuare il login
-     * @param username indirizzo di email
+     * @param email indirizzo di email
      * @param password password
      */
-    private void effettuaLogin(String username, String password) {
+    private void effettuaLogin(String email, String password) {
 
-        mProgress.setVisibility(View.VISIBLE);
 
-        mAuth.signInWithEmailAndPassword(username, password)
+        mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        mProgress.setVisibility(View.INVISIBLE);
+
                         Log.d(TAG, "Task completato: successo = " + task.isSuccessful());
                         if (task.isSuccessful()) {
-                            Intent intent = new Intent(login_activity1.this, MainActivity.class);
+                            Intent intent = new Intent(login.this, MainActivity.class);
                             startActivity(intent);
                         } else {
-                            Toast.makeText(login_activity1.this, R.string.errore_login, Toast.LENGTH_SHORT).show();
+                            Toast.makeText(login.this, R.string.errore_login, Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
     }
 
 }
-
-
-
-
-
-
-
-
-
-
 
